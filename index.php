18b45647f70b3d3df32329621ce68c4791037a05
swethapaturu9 <114616110+swethapaturu9@users.noreply.github.com>
2024-02-18 01:55:17 -0500

    containerizing application

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
             <div class="row it_works">
               <?php
 
-                        $link = mysqli_connect('172.20.1.101', 'ecomuser', 'ecompassword', 'ecomdb');
+                        $link = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
 
                         if ($link) {
                         $res = mysqli_query($link, "select * from products;");
